200d3271938e3f0f54808e2b0bbb44c8b617120e
gusta <valenciotofolo@usp.br>
2021-10-27 20:55:09 -0300

    Commit 09

--- MAC0110 - IBI5011/EPs/EP05.py
+++ MAC0110 - IBI5011/EPs/EP05.py
@@ -1,3 +1,22 @@
 n = int(input('n: '))
 while not n > 0:
     n = int(input('n: '))
+
+a = 1
+eh_soma = False
+
+"""
+# Essa solução já é decente, mas podemos melhorá-la
+while a < n:
+    b = a + 1
+    while b < n:
+        c = b + 1
+        while c ** 2 < a ** 2 + b ** 2:
+            c += 1
+        if a ** 2 + b ** 2 == c ** 2 and a + b + c == n:
+            eh_soma = True
+        b += 1
+    a += 1
+"""
+
+print(eh_soma)
