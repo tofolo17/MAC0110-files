4895c8efbf717e53c73bcb329b04ec5885dd3f81
gusta <valenciotofolo@usp.br>
2021-11-03 19:53:59 -0300

    Commit 12

--- MAC0110 - IBI5011/EPs/EP06.py
+++ MAC0110 - IBI5011/EPs/EP06.py
@@ -1,16 +1,16 @@
 # ----------------------------------------------------
-def primo(n):
+def primo(n: int) -> bool:
     """(int) -> bool
 
        RECEBE um número inteiro n.
        RETORNA True se n é primo e False em caso contrário.
     """
-    if n != 2 and n % 2 == 0:
+    if n != 2 and n % 2 == 0 or not n > 1:
         eh_primo = False
     else:
         i = 3
         eh_primo = True
-        while i < n // 2 and not eh_primo:
+        while i < n // 2 and eh_primo:
             if n % i == 0:
                 eh_primo = False
             i += 2
@@ -19,21 +19,27 @@ def primo(n):
 
 
 # ----------------------------------------------------
-def goldbach(n):
+def goldbach(n: int) -> (bool, int, int):
     """(int) -> bool, int, int
 
        RECEBE um número inteiro n.
        RETORNA True, p, q se n é soma de dois números primos p e q.
        Se n não é soma de dois números primos a função retorna False, 1, 1.
     """
+    i = 2
+    while i < n:
+        if primo(i) and primo(n - i):
+            return True, i, n - i
+        i += 1
+
+    return False, 1, 1
 
 
 # ----------------------------------------------------
-def exponencial(n0, e, p, d):
+def exponencial(n0: int, e: int, p: float, d: int) -> int:
     """(int, int, float, int) -> int
 
        RECEBE
-
          * o número n0 de indivíduos infectados em um dia 0;
          * o número diário médio e de indivíduos com quem alguém
            infectado é exposto;
@@ -43,7 +49,7 @@ def exponencial(n0, e, p, d):
       RETORNA o número total de indivíduos infectados até o dia d
       determinado por (n0, e, p).
     """
-    print("Vixe! Ainda não fiz a função exponencial()")
+    return int(((1 + e * p) ** d) * n0)
 
 
 # --------------------------------------------------
@@ -63,4 +69,14 @@ def logistica(n0, e, p, n, d):
        determinado por (n0, e, p, n).
 
     """
-    print("Vixe! Ainda não fiz a função logistica()")
+    i = 0
+    s = n0
+    while i < d:
+        print(s)
+        s = (1 + e * p * (1 - s / n)) * s
+        i += 1
+
+    return s
+
+
+print(logistica(1, 5, 0.1, 10000, 2))
---
+++ MAC0110/Aula 20/Bubble Sort.py
@@ -0,0 +1,14 @@
+from random import randint
+
+v = [randint(1, 10) for _ in range(1, 10)]
+i = len(v) - 1
+
+while i > 0:
+    j = 0
+    while j < i:
+        if v[j] > v[j + 1]:
+            temp = v[j + 1]
+            v[j + 1] = v[j]
+            v[j] = temp
+        j += 1
+    i -= 1
